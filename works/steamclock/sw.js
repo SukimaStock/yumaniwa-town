2fd19a1096084eb81901fd4d4513a77a4a447a36
SukimaStock <262211877+SukimaStock@users.noreply.github.com>
2026-09-11 12:02:15 +0200

    update

--- works/steamclock/sw.js
+++ works/steamclock/sw.js
@@ -1,6 +1,6 @@
 // SteamClock PWA service worker
 // Bump CACHE_VERSION when shipping a new release that should replace cached files.
-const CACHE_VERSION = "steamclock-v4";
+const CACHE_VERSION = "steamclock-v5";
 
 const PRECACHE_URLS = [
   "./",
